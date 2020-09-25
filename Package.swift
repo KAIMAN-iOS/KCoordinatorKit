20bb96f4f55c366fbeba61e97287bd6ee4a14d03
GG <jerometonnelier@yahoo.fr>
2020-09-25 11:48:48 +0200

    Rename Package

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "K-CoordinatorKit",
+    name: "KCoordinatorKit",
     platforms: [.iOS("12.0")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "K-CoordinatorKit",
-            targets: ["K-CoordinatorKit"]),
+            name: "KCoordinatorKit",
+            targets: ["KCoordinatorKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,11 +20,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "K-CoordinatorKit",
+            name: "KCoordinatorKit",
             dependencies: []),
         .testTarget(
-            name: "K-CoordinatorKitTests",
-            dependencies: ["K-CoordinatorKit"]),
+            name: "KCoordinatorKitTests",
+            dependencies: ["KCoordinatorKit"]),
     ],
     swiftLanguageVersions: [.v5]
 )
